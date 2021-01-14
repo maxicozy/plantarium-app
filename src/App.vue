cdc51be8f25fa0c37af23d95ccf168ee0b691ccc
maxicozy <max@suedien.de>
2021-01-14 13:01:51 +0100

    fixed sticky problem

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
   <div id="app">
     <router-view />
-    <v-bottom-navigation v-model="value" absolute grow height=59 background-color=#c9d8e4 color= #6facd9>
-      <v-btn class="uselectable" @click="push('/calendar')" icon>
+    <v-bottom-navigation :ripple="false" class="unselectable" v-model="value" absolute grow height=59 background-color=#c9d8e4 color= #6facd9>
+      <v-btn @click="push('/calendar')" icon>
         <v-icon>{{ calendarMonthOutlinePath }}</v-icon>
       </v-btn>
-      <v-btn class="uselectable" @click="push('/')" icon>
+      <v-btn @click="push('/')" icon>
         <v-icon>{{ sproutPath }}</v-icon>
       </v-btn>
-      <v-btn class="uselectable" @click="push('/settings')" icon>
+      <v-btn @click="push('/settings')" icon>
         <v-icon>{{ cogPath }}</v-icon>
       </v-btn>
     </v-bottom-navigation>
@@ -16,13 +16,13 @@
 </template>
 
 <script>
-  import { mdiSprout, mdiCog, mdiCalendarMonthOutline } from '@mdi/js'
+  import { mdiSprout, mdiCog, mdiCalendarMonthOutline} from '@mdi/js';
   export default {
     data: () => ({
       value: 'recent',
       sproutPath: mdiSprout,
       cogPath: mdiCog,
-      calendarMonthOutlinePath: mdiCalendarMonthOutline
+      calendarMonthOutlinePath: mdiCalendarMonthOutline,
     }),
   }
 </script>
@@ -35,13 +35,8 @@
     text-align: center;
     color: #2c3e50;
   }
-  .unselectable {
-    -webkit-touch-callout:none;
-    -webkit-tap-highlight-color:rgba(0,0,0,0);
-    -moz-user-select: -moz-none;
-    -khtml-user-select: none;
-    -webkit-user-select: none;
-    -o-user-select: none;
-    user-select: none;
+
+  .unselectable .theme--light.v-btn:hover::before {
+    opacity: 0;
   }
 </style>
